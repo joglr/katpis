1a21d280dcc0204c47dc57f1a93433cc473dc7a1
Jonas Glerup Røssum <jonas.g.roessum@gmail.com>
2020-11-04 09:27:10 +0100

    Release version 0.3.0 (#11)
    Add validity indicator
    Validate email on the fly
    Update Program.cs
    
    Co-authored-by: Interrobangs <35430583+Interrobangs@users.noreply.github.com>

--- Program.cs
+++ Program.cs
@@ -1,6 +1,5 @@
 ﻿using System.Net.Mail;
 using System.IO;
-using System.Text.RegularExpressions;
 using System;
 using System.Threading.Tasks;
 using static System.ConsoleColor;
@@ -12,45 +11,55 @@ namespace kat
     const ConsoleColor FOREGROUND_COLOR = White;
     const ConsoleColor BACKGROUND_COLOR = Black;
     const ConsoleColor HINT_COLOR = Cyan;
-    const ConsoleColor PREDICTION_COLOR = DarkGray;
     const string COMMON_EMAIL_SUFFIXES_PATH = @"Resources\email-suffixes.csv";
     const string validCharsPattern = @"^\S$";
 
     public static string[] commonEmailSuffixes;
 
+    public static Func<string, string> emailPredicter = text =>
+    {
+      if (text.Contains("@") && (text.Split("@").Length > 1))
+      {
+        var parts = text.Split("@");
+        if (parts.Length.Equals(2))
+        {
+          var server = parts[1];
+          if (server.Length > 0)
+          {
+            foreach (var item in commonEmailSuffixes)
+            {
+              if (item.StartsWith(server) && item.Contains(server) && !item.Equals(server))
+              {
+                return item.Substring(server.Length);
+              }
+            }
+          }
+        }
+      }
+      return null;
+    };
 
     static async Task Main(string[] args)
     {
-
-      Util.Colored(() => Console.Write("Loading..."));
-      ClearCurrentConsoleLine();
+      ConsoleUtils.Colored(() => Console.Write("Loading..."));
+      ConsoleUtils.ClearCurrentConsoleLine();
       commonEmailSuffixes = await File.ReadAllLinesAsync(COMMON_EMAIL_SUFFIXES_PATH);
       Console.Title = "Kat CLI";
 
       string email = null;
 
-      do
+      ConsoleUtils.Colored(() =>
       {
-        ClearCurrentConsoleLine();
-        Util.Colored(() =>
-        {
-          email = AcceptInput(message: email == null
-              ? "Enter your email: "
-              : "Invalid email, try again");
-        });
-      } while (!VerifyEmail(email: email));
-
-      ClearCurrentConsoleLine();
+        email = ConsoleUtils.AcceptInput(
+          helpMessage: "Enter your email: ",
+          errorMessage: "Invalid email, enter your email: ",
+          checkValid: VerifyEmail,
+          getPrediction: emailPredicter,
+          acceptedCharsPattern: validCharsPattern
+        );
+      });
+      Console.WriteLine();
       Console.WriteLine(email);
-    }
-
-    private static void ApplyColors(
-      ConsoleColor foregroundColor = FOREGROUND_COLOR,
-      ConsoleColor backgroundColor = BACKGROUND_COLOR
-    )
-    {
-      Console.ForegroundColor = foregroundColor;
-      Console.BackgroundColor = backgroundColor;
     }
 
     private static bool VerifyEmail(string email)
@@ -65,108 +74,6 @@ namespace kat
         return false;
       }
       return true;
-    }
-
-    public static void ClearCurrentConsoleLine()
-    {
-      int currentLineCursor = Console.CursorTop;
-      Console.SetCursorPosition(0, Console.CursorTop);
-      for (int i = 0; i < Console.WindowWidth; i++)
-        Console.Write(" ");
-      Console.SetCursorPosition(0, currentLineCursor);
-    }
-
-    private static string AcceptInput(string message, String acceptedCharsPattern = validCharsPattern)
-    {
-      string status = "";
-      string text = "";
-      string prediction = null;
-
-      bool enterPressed = false;
-
-      while (!enterPressed)
-      {
-
-        Util.Colored(() =>
-        {
-          ClearCurrentConsoleLine();
-          Console.Write(message);
-          Console.Write(text);
-        });
-
-        if (prediction != null)
-        {
-          var cl = Console.CursorLeft;
-          var ct = Console.CursorTop;
-          Util.Colored(() => Console.Write(prediction), foregroundColor: PREDICTION_COLOR);
-          Console.SetCursorPosition(cl, ct);
-        }
-        ConsoleKeyInfo cki = Console.ReadKey();
-
-        switch (cki.Key)
-        {
-          case ConsoleKey.Backspace:
-            if (text.Length > 0)
-            {
-              status = "backspace";
-              text = text.Substring(0, text.Length - 1);
-            }
-            break;
-
-          case ConsoleKey.Enter:
-            enterPressed = true;
-            break;
-
-          case ConsoleKey.Tab:
-            if (prediction != null)
-            {
-              text += prediction;
-              prediction = null;
-            }
-            break;
-
-          case ConsoleKey.UpArrow:
-          case ConsoleKey.DownArrow:
-          case ConsoleKey.LeftArrow:
-          case ConsoleKey.RightArrow:
-
-            break;
-
-          default:
-            if (new Regex(validCharsPattern).IsMatch(cki.KeyChar.ToString()))
-            {
-              status = "legal character: \"" + cki.KeyChar + "\"";
-              text += cki.KeyChar;
-              text = text.Trim();
-            }
-            else status = "non-allowed character or other key: " + cki.Key.ToString();
-            break;
-        }
-
-        prediction = null;
-
-        if (text.Contains("@") && (text.Split("@").Length > 1))
-        {
-          var parts = text.Split("@");
-          if (parts.Length.Equals(2))
-          {
-            var server = parts[1];
-            if (server.Length > 0)
-            {
-              foreach (var item in commonEmailSuffixes)
-              {
-                if (item.StartsWith(server) && item.Contains(server) && !item.Equals(server))
-                {
-                  prediction = item.Substring(server.Length);
-                  break;
-                }
-              }
-            }
-          }
-        }
-      }
-
-      return text;
     }
   }
 }
