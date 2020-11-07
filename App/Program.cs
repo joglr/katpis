80cc4a22e1557a68f776e72685a6d730d8a26221
joglr <jonas.g.roessum@gmail.com>
2020-11-07 20:29:10 +0100

    Restructure repository

---
+++ App/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics;
+using System.IO;
+using System;
+
+namespace Tester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length >= 1) {
+                if (args[0].Equals("test")) {
+                    Console.WriteLine("Testing");
+                    var cd = System.Environment.CurrentDirectory;
+                    Console.WriteLine(cd);
+
+
+
+                    // (later) Download test files
+                    // 1. Reads .in file(s) from tests
+
+
+
+                    // Pipe infile into program
+                        // Determine file extension
+                        // Map to runner
+                    // Get result
+                    // Read output of program
+                    // Compare to .ans file(s)
+                    // Output differences or accepted
+                }
+            }
+        }
+    }
+}
